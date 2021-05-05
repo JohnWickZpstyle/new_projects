9159afc86ba97a5d1bf50bf243e6166025073c6c
John Bow <farmermid2288@gmail.com>
2021-05-06 01:23:05 +0300

    Delete comment

--- сurrency_сonverter.py
+++ сurrency_сonverter.py
@@ -22,28 +22,3 @@ def main():
 
 if __name__ == '__main__':
     print(main())
-
-
-
-'''
-
-import requests
-import json
-
-def main():
-
-    inp_value = input("Введите валютные единицы в сокращенном варианте (EUR,RUB через запятую) первое это то что хотите"
-                      " перевести и второе во что хотите перевести, нажмите Enter:" )
-    
-    number_of_cash_units = int(input("Колличество единиц которыое вы имеете: "))
-
-    ready_currency = round(float(requests.get(f"https://currate.ru/api/?get=rates&pairs={inp_value},"
-                                              f"{inp_value}&key=7bb14fda0ea69a44a2244af27297d43a").json()['data'][
-                                     inp_value]) * number_of_cash_units, 3)
-    return ready_currency
-
-if __name__ == '__main__':
-    main()
-
-
-'''
